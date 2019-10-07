dc53c2c6beaac35c814b31edaa47829298b91ea9
Amel Ramirez <amelramirez@192.168.2.15>
2019-10-07 18:01:26 +0200

    header layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.scss';
-import FullLogo from './img/ac-full-logo.png';
-// import Logo from './components/logo';
-
+import Header from './components/header/header';
 function App() {
   return (
     <div className="App">
-      <img className='full-logo' src={FullLogo} alt='logo' />
-      <h1>hello</h1>
+      <Header />
     </div>
   );
 }
