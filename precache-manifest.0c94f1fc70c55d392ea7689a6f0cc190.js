12b2ef70626ef9a3ccd2b47b843596593e223999
System Administrator <root@192.168.2.15>
2019-10-15 15:22:47 +0200

    Updates

---
+++ precache-manifest.0c94f1fc70c55d392ea7689a6f0cc190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabaea369671f974363e31f25a826bd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2c0cd0b3668e8ac23e5",
+    "url": "/static/css/main.6df9b093.chunk.css"
+  },
+  {
+    "revision": "206be52f617473ec3bf7",
+    "url": "/static/js/2.2f988b1a.chunk.js"
+  },
+  {
+    "revision": "d2c0cd0b3668e8ac23e5",
+    "url": "/static/js/main.eb693371.chunk.js"
+  },
+  {
+    "revision": "10a5446d78f04ed061b3",
+    "url": "/static/js/runtime~main.cdc30ecc.js"
+  }
+]);
